d2e5f6baa8886665dec0b746d8d05b962939d043
Narayan Gautam <46322720+gnarayyan@users.noreply.github.com>
2025-07-21 13:56:04 +0545

    Enhance: enable core library desugaring and multiDex support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,8 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
+        // Flag to enable support for the new language APIs
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -20,6 +22,7 @@ android {
     }
 
     defaultConfig {
+        multiDexEnabled = true
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.next_todo"
         // You can update the following values to match your application needs.
